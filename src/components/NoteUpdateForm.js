bf7490268b229e02c1dcecb731399f702ebea7d4
Aaron <aaronmeade9292@gmail.com>
2018-08-11 16:47:45 -0700

    finished up

--- src/components/NoteUpdateForm.js
+++ src/components/NoteUpdateForm.js
@@ -1,10 +1,39 @@
 import React, {Component, Fragment} from 'react';
 
-export default props => {
+export default class NoteUpdateForm extends Component {
+    state = {
+        note: this.props.note,
+    }
+
+    noteUpdate = (e) => {
+        e.preventDefault();
+        console.log(this.state.note.content);
+        const firstNote = {...this.state.note}
+        firstNote.content = this.state.note.content;
+        this.props.update(firstNote)
+        this.setState({
+            note: {content: ''}
+        })
+        
+    }
+
+    handleChange = (e) => {
+        e.preventDefault();
+        const changedNote = {...this.state.note}
+        changedNote.content = e.target.value
+        this.setState({
+          note: changedNote,
+        });
+      }
+
+    render() {
         return(
             <React.Fragment>
-            <textarea>Edit</textarea>
-            <button onClick={() => {props.updated(props.note)}}></button>
+            <form onSubmit={this.noteUpdate}>
+            <textarea type='text' value={this.state.content} onChange={this.handleChange}>Edit</textarea>
+            <button>Edit</button>
+            </form>
             </React.Fragment>
         )
+    }
 }
